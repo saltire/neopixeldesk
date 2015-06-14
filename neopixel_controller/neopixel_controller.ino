03a0087ac23b68c1f9196d5b1c52ee774e592173
saltire sable <saltiresable@gmail.com>
2015-06-14 09:48:53 -0400

    Add fade effect

--- neopixel_controller/neopixel_controller.ino
+++ neopixel_controller/neopixel_controller.ino
@@ -2,7 +2,7 @@
 #include <avr/power.h>
 
 #define PIN 6
-#define PIXEL_COUNT 150
+#define NUM_PIXELS 150
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -11,13 +11,14 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN, NEO_GRB & NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
+    setColor(0, 255, 0);
+    colorFade(255, 0, 0, 1000, 20);
     Serial.begin(9600);
-    colorWipe(strip.Color(50, 50, 50), 0);
 }
 
 void loop() {
@@ -25,8 +26,16 @@ void loop() {
         uint8_t r = Serial.read();
         uint8_t g = Serial.read();
         uint8_t b = Serial.read();
-        colorWipe(strip.Color(g, r, b), 0);
+        colorFade(r, g, b, 1000, 20);
     }
+}
+
+// Fill all the dots with a color simultaneously
+void setColor(uint8_t r, uint8_t g, uint8_t b) {
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, r, g, b);
+    }
+    strip.show();
 }
 
 // Fill the dots one after the other with a color
@@ -35,6 +44,32 @@ void colorWipe(uint32_t c, uint8_t wait) {
         strip.setPixelColor(i, c);
         strip.show();
         delay(wait);
+    }
+}
+
+void colorFade(uint8_t cr, uint8_t cg, uint8_t cb, uint16_t duration, uint16_t step) {
+    if (duration == 0) {
+        setColor(cr, cg, cb);
+        return;
+    }
+    if (step > duration) step = duration;
+
+    uint8_t *pixels = strip.getPixels();
+    for (uint16_t s = duration / step; s > 0; s--) {
+        for (uint16_t p = 0; p < NUM_PIXELS; p++) {
+            uint8_t *pixel = &pixels[p * 3];
+            uint8_t pr = pixel[1];
+            uint8_t pg = pixel[0];
+            uint8_t pb = pixel[2];
+
+            uint8_t r = (cr > pr) ? (pr + (cr - pr) / s) : (pr - (pr - cr) / s);
+            uint8_t g = (cg > pg) ? (pg + (cg - pg) / s) : (pg - (pg - cg) / s);
+            uint8_t b = (cb > pb) ? (pb + (cb - pb) / s) : (pb - (pb - cb) / s);
+
+            strip.setPixelColor(p, r, g, b);
+        }
+        strip.show();
+        delay(step);
     }
 }
 
@@ -54,4 +89,3 @@ uint32_t Wheel(byte WheelPos) {
         return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
     }
 }
-
